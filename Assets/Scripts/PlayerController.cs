b0c36ca6b30a1dd8475374838e40969b102e1571
PaintedPorch <lars.bakker@casema.nl>
2023-04-17 11:01:04 +0200

    Add Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float rotateSpeed = 250f;
+    [SerializeField] float rotateSpeed = 250f; 
+    [SerializeField] bool isGround = false;
+    [SerializeField] float jumpForce = 2500f;
+    Rigidbody rigidBody;
+
+    void Start() 
+    {
+        rigidBody = GetComponent<Rigidbody>();    
+    }
+
     void Update()
     {
         float verticalAmount = Input.GetAxisRaw("Vertical");
@@ -16,5 +25,17 @@ public class PlayerController : MonoBehaviour
        transform.Translate(new Vector3(horizontalAmount * moveSpeed * Time.deltaTime, 0, 0));
 
        transform.Rotate(new Vector3(0, horizontalRotation * rotateSpeed * Time.deltaTime, 0));
+
+       if (Input.GetKeyDown("space") && isGround == true) {
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+       }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Ground")
+        {
+            isGround = true; 
+        }
     }
 }
