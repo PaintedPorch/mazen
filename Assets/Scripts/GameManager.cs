ad50349562c2fc5c9c76230a93ee16a8aa196bec
PaintedPorch <lars.bakker@casema.nl>
2023-04-25 11:48:56 +0200

    Add Restart-After-Death Functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,14 @@ public class GameManager : MonoBehaviour
     PlayerController playerController;
     [SerializeField] TextMeshProUGUI gameOverText;
     [SerializeField] Button restartButton;
+    [SerializeField] Image gameOverMenuBg;
     // Start is called before the first frame update
     void Start()
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         gameOverText.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(false);
+        gameOverMenuBg.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,12 +28,18 @@ public class GameManager : MonoBehaviour
 
     void GameOver() {
         if (playerController.gameOver) {
+            StartCoroutine(GameOverScreenDelay());
             gameOverText.gameObject.SetActive(true);
             restartButton.gameObject.SetActive(true);
+            gameOverMenuBg.gameObject.SetActive(true);
         }
     }
 
     public void RestartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator GameOverScreenDelay() {
+        yield return new WaitForSeconds(2);
     }
 }
